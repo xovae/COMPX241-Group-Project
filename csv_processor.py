35fcdfad111725f2212f72c7acff4c5e6f3db58e
Jamie Boulton <jamie.william.boulton@gmail.com>
2025-04-28 20:00:36 +1200

    Prototype version (initial commit)
    Currently issues:
    Websites with phrases incorrectly concatenated such as 'Emails' are removed, but so are any other websites with capital letters other than the first letter
    Final words in a sentence are sometimes flagged, e.g. "emails." is counted as a match
    Descriptions are often empty

---
+++ csv_processor.py
@@ -0,0 +1,76 @@
+import csv
+import re
+import json
+
+#Open file
+with open('Warnings_and_Alerts_FMA_all.csv', encoding='utf-8-sig') as fileObject:
+    
+    #Create reader object
+    reader = csv.reader(fileObject)
+    
+    #Skip the headings row
+    next(reader)
+    
+    #Construct array to contain json entries to be written to an output file
+    json_entries = []
+    
+    for row in reader:
+        name = row[0]
+        date = row[1]
+        content = row[2]
+        
+        #Get start of description
+        description_start = content.find("FSPR: \n")
+        
+        #If start is found
+        if description_start != -1:
+            
+            #Attempt to find first common end of the description
+            description_end = content.find("Entity")
+            
+            #If it isn't present, attempt to find the other most common end of the description
+            if description_end == -1:
+                description_end = content.find("Websites")
+            
+            #If found, capture all content between the description start and end
+            if description_end != -1:
+                description = content[description_start + len(description_start):description_end]
+            #Otherwise, capture everything past the description start
+            else:
+                description = content[description_start + len(description_start):]
+        else: 
+            description = ""
+            
+        # websites = re.findall(r'\b(?:[@a-zA-Z0-9-]+\.(?=[^.]))+[a-zA-Z]{2,}\b', content)
+        # websites = re.findall(r'\b(?:[@a-zA-Z0-9-]+\.(?=[^.]))+[a-zA-Z]{2,}(?!\.)\b', content)
+        websites = [valid for valid in websites if "@" not in valid]
+        
+        if len(websites) != 0: 
+            
+            #Remove duplicates
+            websites = list(set(websites))
+            
+            #Remove trailing phrases e.g. 'Emails'
+            sorted_websites = []
+            
+            for entry in websites:
+                entry = entry[0].lower() + entry[1:]
+                string = re.split(r'[A-Z]', entry)[0]
+                sorted_websites.append(string)
+                
+            #Remove duplicates
+            sorted_websites = list(set(sorted_websites))
+            
+            entry = {
+                "Name" : name,
+                "Date": date,
+                "Description": description,
+                "Websites": sorted_websites
+            }
+            
+            json_entries.append(entry)
+        
+    with open('output.json', 'w', encoding='utf-8') as file:
+        json.dump(json_entries, file, indent = 2)
+        
+        
