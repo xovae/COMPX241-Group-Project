89e66c5b93b39c5a192501a61361ecdb2672dd19
Jamie Boulton <jamie.william.boulton@gmail.com>
2025-05-21 10:23:31 +1200

    Updated many variable names to use camelCase instead of underscores. FMA alerts are now processed before ASIC alerts due to having more information on scams listed compared to an equivalent ASIC alert. Added website caching to both the ASIC and IOSCO processor to prevent duplicates across any entry regardless of source. To achieve this, the website cache has been made global across all 3 data sources, rather than making it exclusive to the FMA processor. Updated logic in the FMA processor the be more simplified, and fixed the checking of whitelisted entries. Simplified some logic in the ASIC processor, as well as adding the "otherInformationGeneral" field to JSON entries in the "Description" field, if it isn't empty. Also added stripping of prefixes ("www.", "http://", and "https:") to both the ASIC and IOSCO processors.

--- alerts_processor.py
+++ alerts_processor.py
@@ -5,12 +5,18 @@ import re
 import requests
     
 #Stores all json entries to be written to an output file
-scams_list = []
+scamsList = []
 
 #Stores all websites found to prevent duplicates between entries
-website_cache = []
-    
-def ASIC_processor():
+websiteCache = []
+
+#Safe websites that are included in descriptions occasionally
+whitelistedWebsites = ("netsafe.org.nz", "seek.com.au", "fca.org.uk", "nz.linkedin.com", "sorted.org.nz", "baidu.com", "co.nz", "sohu.com", "zhihu.com", "kiwibank.co.nz", "twitter.com", "youtube.com", "linkedin.com", "facebook.com", "en-gb.facebook.com")
+
+#Safe TLDs that are included in descriptions occasionally
+whitelistedTLDs = ("govt.nz", "gov.au")
+   
+def ASICProcessor():
     #Open file
     with open('investor-alert-list.json', encoding='utf-8-sig') as fileObject:
         
@@ -19,24 +25,58 @@ def ASIC_processor():
 
         #For every JSON file
         for entry in jsonData:
-            #Protects against some None types in the JSON file
-            if entry.get("websites") != None:
-                #Filters out JSON entries with no websites listed
-                if entry["websites"][0]:
+            #Filter out empty entries or ones valued None in the JSON file
+            if entry.get("websites") and entry["websites"][0]:
+                    #Get date of listing
                     if entry["dateAdded"] != None:
                         date = entry["dateAdded"]
                     else:
                         date = entry["dateUpdated"]
-                    filtered_entry = {
-                        "Type" : "AUS",
-                        "Name" : entry["nameMandatory"],
-                        "Date": date,
-                        "Websites": entry["websites"]
-                    }
-                    
-                    scams_list.append(filtered_entry)
-        
-def FMA_processor():
+                        
+                    #Get additional info if present
+                    if entry["otherInformationGeneral"] != "":
+                        description = entry["otherInformationGeneral"]
+                    else:
+                        description = None
+                        
+                    newWebsites = []
+                        
+                    for website in entry["websites"]:
+                        #Remove prefixes
+                        filteredWebsite = website.replace("www.", "")
+                        filteredWebsite = filteredWebsite.replace("http://", "")
+                        filteredWebsite = filteredWebsite.replace("https://", "")
+                        
+                        #Filter whitelisted entries
+                        if filteredWebsite.endswith(whitelistedTLDs) == False and filteredWebsite not in whitelistedWebsites:
+                            #Check if the website has been listed in previous entries
+                            if filteredWebsite not in websiteCache and filteredWebsite != "":
+                                websiteCache.append(filteredWebsite)
+                                newWebsites.append(filteredWebsite)
+                              
+                    #Add the entry only if there are new websites listed                              
+                    if len(newWebsites) != 0:
+                        
+                        #Construct JSON entry with fields depending on if "otherInformationGeneral" had content or not
+                        if description != None:
+                            filteredEntry = {
+                                "Type" : "AUS",
+                                "Name" : entry["nameMandatory"],
+                                "Date": date,
+                                "Description": description,
+                                "Websites": newWebsites
+                            }
+                        else:
+                            filteredEntry = {
+                                "Type" : "AUS",
+                                "Name" : entry["nameMandatory"],
+                                "Date": date,
+                                "Websites": newWebsites
+                            }
+                        
+                        scamsList.append(filteredEntry)
+
+def FMAProcessor():
     #Download the latest copy of the CSV file
     url = "https://www.fma.govt.nz/library/warnings-and-alerts/downloadWarnings/?date=all"
     response = requests.get(url)
@@ -53,42 +93,34 @@ def FMA_processor():
     #Skip the headings row
     next(reader)
 
-    #Safe websites that are included in descriptions occasionally
-    whitelistedWebsites = ("netsafe.org.nz", "seek.com.au", "fca.org.uk", "nz.linkedin.com", "sorted.org.nz", "baidu.com", "co.nz", "sohu.com", "zhihu.com", "kiwibank.co.nz", "twitter.com", "youtube.com", "linkedin.com")
-
-    #Safe TLDs that are included in descriptions occasionally
-    whitelistedTLDs = ("govt.nz", "gov.au", "facebook.com")
-
-    #Common websites listed in scam descriptions that aren't scams
-
     for row in reader:
         name = row[0]
         date = row[1]
         content = row[2]
         
         #Get start of description
-        description_start = content.find("FSPR:")
+        descriptionStart = content.find("FSPR:")
         
         #If start is found
-        if description_start != -1:
+        if descriptionStart != -1:
             
             #Used to filter out incorrect captures of websites in the headings
-            websiteCapture = content[description_start + len("FSPR:  "): ]
+            websiteCapture = content[descriptionStart + len("FSPR:  "): ]
             websites = re.findall(r'\b(?:[@a-zA-Z0-9-]+\.(?=[^.]))+[a-zA-Z]{2,}\b', websiteCapture)
             
             #Attempt to find first common end of the description
-            description_end = content.find("Entity")
+            descriptionEnd = content.find("Entity")
             
             #If it isn't present, attempt to find the other most common end of the description
-            if description_end == -1:
-                description_end = content.find("Websites")
+            if descriptionEnd == -1:
+                descriptionEnd = content.find("Websites")
         
             #If an description end is found, capture all content between the description start and end, stripping any trailing whitespace
-            if description_end != -1:
-                description = content[description_start + len("FSPR:   "):description_end].rstrip()
+            if descriptionEnd != -1:
+                description = content[descriptionStart + len("FSPR:   "):descriptionEnd].rstrip()
             #Otherwise, capture everything past the description start, stripping any trailing whitespace
             else:
-                description = content[description_start + len("FSPR:   "):].rstrip()
+                description = content[descriptionStart + len("FSPR:   "):].rstrip()
         else: 
             description = ""
             websites = re.findall(r'\b(?:[@a-zA-Z0-9-]+\.(?=[^.]))+[a-zA-Z]{2,}\b', content)
@@ -96,20 +128,25 @@ def FMA_processor():
         websites = [valid for valid in websites if "@" not in valid]
         
         if len(websites) != 0: 
+            #Array to list all new websites not mentioned by previous entries
+            newWebsites = []
             
             #Remove duplicates
             websites = list(set(websites))
             
-            for i in reversed(range(len(websites))):
+            for website in websites:
                 #Remove trailing phrases in the TLD e.g. '.comEmails'
-                tldIndex = websites[i].rfind(".")
-                filteredTLD = re.split(r'[A-Z]', websites[i][tldIndex:])[0]
-                filteredWebsite = (websites[i][:tldIndex] + filteredTLD).lower()
+                tldIndex = website.rfind(".")
+                filteredTLD = re.split(r'[A-Z]', website[tldIndex:])[0]
+                filteredWebsite = (website[:tldIndex] + filteredTLD).lower()
+                #Remove prefixes
                 filteredWebsite = filteredWebsite.replace("www.", "")
-                    
+                filteredWebsite = filteredWebsite.replace("http://", "")
+                filteredWebsite = filteredWebsite.replace("https://", "")
+                
                 validWebsite = False
                 
-                #Check the website is valid (contains a valid TLD)
+                #Filter websites by checking if the TLD is valid. Strips a TLD every time it's invalid until the string is no longer a valid website
                 while len(filteredWebsite.split('.')) > 1:
                     if filteredWebsite.split('.')[1] != "":
                         if filteredWebsite.split('.')[-1] in validTLDs:
@@ -123,50 +160,39 @@ def FMA_processor():
                                 break
                     else:
                         break
-            
+                    
                 if validWebsite == True:
-                    #Remove whitelist entries
-                    if filteredWebsite.endswith(whitelistedTLDs) == True or filteredWebsite in whitelistedWebsites:
-                        del websites[i]
-                    else:
+                    #Filter whitelisted entries
+                    if filteredWebsite.endswith(whitelistedTLDs) == False and filteredWebsite not in whitelistedWebsites:
                         #Remove echoed entries
                         length = len(filteredWebsite) - 1
                         if length % 2 == 0:
                             midpoint = len(filteredWebsite) // 2
                             if filteredWebsite[:midpoint] == filteredWebsite[midpoint+1:]:
-                                websites[i] = filteredWebsite[:midpoint]
-                            else:
-                                websites[i] = filteredWebsite
-                        else:
-                            websites[i] = filteredWebsite
-                else:
-                    del websites[i]
-                
-            new_websites = []
-                
-            for website in websites:
-                #Check if the website has been listed before
-                if website not in website_cache:
-                    website_cache.append(website)
-                    new_websites.append(website)
-                    
-            #Add the entry if there are new websites listed
-            if len(new_websites) != 0:
+                                filteredWebsite = filteredWebsite[:midpoint]
+                                
+                        #Check if the website has been listed in previous entries
+                        if filteredWebsite not in websiteCache:
+                            websiteCache.append(filteredWebsite)
+                            newWebsites.append(filteredWebsite)
+                            
+            #Add the entry only if there are new websites listed
+            if len(newWebsites) != 0:
 
                 # Remove any duplicates revealed by TLD filtering
-                websites = list(set(new_websites))
+                newWebsites = list(set(newWebsites))
                          
                 entry = {
                     "Type" : "NZ",
                     "Name" : name,
                     "Date": date,
                     "Description": description,
-                    "Websites": websites
+                    "Websites": newWebsites
                 }
                 
-                scams_list.append(entry)
-                
-def IOSCO_processor():
+                scamsList.append(entry)
+ 
+def IOSCOProcessor():
     #Download the latest copy of the CSV file
     url = "https://www.iosco.org/i-scan/?export-to-csv&VALIDATIONDATESTART=&page=1&SUBSECTION=main&CATEGORYID=&ID=&VALIDATIONDATEEND=&PRODUCTID=&NCA_ID=&KEYWORDS="
     response = requests.get(url)
@@ -178,45 +204,49 @@ def IOSCO_processor():
     #Skip the headings row
     next(reader)
     
-    no_matches = []
-    
     for row in reader:
         nca = row[2]
         date = row[4]
         name = row[6]
         website = row[9]
         otherWebsites = row[10]
-        domain_name = row[11]
+        domainName = row[11]
         description = row[24]
         
-        if website != "" or otherWebsites != "" or domain_name != "" and (len(websites) != 0 or len(otherWebsites) != 0 or len(domain_name) != 0):
+        #If a website has been listed in any of the applicable fields
+        if website != "" or otherWebsites != "" or domainName != "" and (len(websites) != 0 or len(otherWebsites) != 0 or len(domainName) != 0):
             
-            websites = [website] + otherWebsites.split('|') + [domain_name]
+            #Combine all entries
+            websites = [website] + otherWebsites.split('|') + [domainName]
             
-            new_websites = []
+            newWebsites = []
                 
             for website in websites:
-                #Check if the website has been listed before
-                if website not in website_cache and website != "":
+                
+                #Remove trailing info
+                filteredWebsite = website.replace("www.", "")
+                filteredWebsite = filteredWebsite.replace("http://", "")
+                filteredWebsite = filteredWebsite.replace("https://", "")
+                
+                #Extract website from string
+                match = re.search(r'\b(?:[@a-zA-Z0-9-]+\.(?=[^.]))+[a-zA-Z]{2,}\b', filteredWebsite)
+            
+                #If a valid website is found
+                if match != None:                
+                    filteredWebsite = match.group()
                     
-                    #Remove trailing info
-                    filteredWebsite = website.replace("www.", "")
-                    filteredWebsite = filteredWebsite.replace("http://", "")
-                    filteredWebsite = filteredWebsite.replace("https://", "")
-                    
-                    match = re.search(r'\b(?:[@a-zA-Z0-9-]+\.(?=[^.]))+[a-zA-Z]{2,}\b', filteredWebsite)
-                    
-                    if match != None:                
-                        filteredWebsite = match.group()
-                        
-                        website_cache.append(filteredWebsite)
-                        new_websites.append(filteredWebsite)
+                    #Check if the website is whitelisted
+                    if filteredWebsite.endswith(whitelistedTLDs) == False and filteredWebsite not in whitelistedWebsites:
+                        #Check if the website has been listed before
+                        if filteredWebsite not in websiteCache and filteredWebsite != "":
+                            websiteCache.append(filteredWebsite)
+                            newWebsites.append(filteredWebsite)
                     
             #Add the entry if there are new websites listed
-            if len(new_websites) != 0:
+            if len(newWebsites) != 0:
 
                 # Remove any duplicates
-                websites = list(set(new_websites))
+                newWebsites = list(set(newWebsites))
                             
                 entry = {
                     "Type" : "IOSCO",
@@ -224,23 +254,22 @@ def IOSCO_processor():
                     "Name" : name,
                     "Date": date,
                     "Description": description,
-                    "Websites": websites
+                    "Websites": newWebsites
                 }
                 
-                
-                scams_list.append(entry)
+                scamsList.append(entry)
 
 if __name__ == '__main__':
     
-    print("Processing ASIC!")
-    ASIC_processor()
-    
     print("Processing FMA!")
-    FMA_processor()
+    FMAProcessor()
+    
+    print("Processing ASIC!")
+    ASICProcessor()
     
     print("Processing IOSCO!")
-    IOSCO_processor()
+    IOSCOProcessor()
            
-    with open('scams_list.json', 'w', encoding='utf-8') as file:
-            json.dump(scams_list, file, indent = 2)
+    with open('scamsList.json', 'w', encoding='utf-8') as file:
+            json.dump(scamsList, file, indent = 2)
     
